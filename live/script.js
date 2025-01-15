4bcb0e83ecf1db749c8ff973f0cb2acc6f89e09e
dwtv08 <dwtv.stream@gmail.com>
2025-01-16 02:47:35 +0700

    Update script.js

--- live/script.js
+++ live/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const source = 'https://tvpull.careryun.com/live/ballbar_11270.m3u8';
+  const source = 'https://nflarcadia.xyz/bRtT37sn3w/Sx5q6YTgCs/2.m3u8';
   const video = document.querySelector('video');
 
   // For more options see: https://github.com/sampotts/plyr/#options
