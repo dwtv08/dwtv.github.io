f9c94bfece2aa435e946f714a020cf9fad08d63b
dwtv08 <dwtv.stream@gmail.com>
2025-01-20 02:09:01 +0700

    Update script.js

--- live/p1/script.js
+++ live/p1/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const source = 'https://nflarcadia.xyz/bRtT37sn3w/Sx5q6YTgCs/3.m3u8';
+  const source = 'https://nflarcadia.xyz/bRtT37sn3w/Sx5q6YTgCs/3.m';
   const video = document.querySelector('video');
 
   // For more options see: https://github.com/sampotts/plyr/#options
