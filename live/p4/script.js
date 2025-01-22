aa4d229b72a769cbbb5384ea0155f78030833182
dwtv08 <dwtv.stream@gmail.com>
2025-01-22 14:05:11 +0700

    Update script.js

--- live/p4/script.js
+++ live/p4/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const source = 'https://nflarcadia.xyz/bRtT37sn3w/Sx5q6YTgCs/2.m3u8';
+  const source = 'https://hlslive-tx-cdn.ysp.cctv.cn/ikotv/2026207401.m3u8';
   const video = document.querySelector('video');
 
   // For more options see: https://github.com/sampotts/plyr/#options
